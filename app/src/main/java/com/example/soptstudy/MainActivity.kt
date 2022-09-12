02203d13251f236459540b430be75f807fff6c50
yoon <cbj0010@naver.com>
2022-09-13 03:05:52 +0900

    databinding 전까지

--- app/src/main/java/com/example/soptstudy/MainActivity.kt
+++ app/src/main/java/com/example/soptstudy/MainActivity.kt
@@ -1,37 +1,34 @@
 package com.example.soptstudy
 
-import android.media.Image
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.example.soptstudy.databinding.ActivityMainBinding
-import java.util.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var diceImage: Image
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.rollButton.text = "Let's Roll"
-        binding.rollButton.setOnClickListener {
-            rollDice()
+        binding.button.setOnClickListener {
+            addNickName()
         }
     }
 
-    private fun rollDice() {
-        val raandomInt = Random().nextInt(6) + 1
-        val drawableResource = when (raandomInt) {
-            1 -> R.drawable.dice_1
-            2 -> R.drawable.dice_2
-            3 -> R.drawable.dice_3
-            4 -> R.drawable.dice_4
-            5 -> R.drawable.dice_5
-            else -> R.drawable.dice_6
+    private fun addNickName() {
+
+        binding.apply {
+            tvShoew.text = binding.editTextTextPersonName.text
+            invalidateAll() //새로운걸 ui 업데이트 하기 위해서
+            editTextTextPersonName.visibility = View.GONE
+            button.visibility = View.GONE
+
         }
-        binding.diceImage.setImageResource(drawableResource)
+
     }
 
 }
+
