ffa3f95c9cc3e5c16b1ecd4f919a5349e00753c6
yoon <cbj0010@naver.com>
2022-09-13 01:34:43 +0900

    Build your First App

--- app/src/main/java/com/example/soptstudy/MainActivity.kt
+++ app/src/main/java/com/example/soptstudy/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.soptstudy
 
-import androidx.appcompat.app.AppCompatActivity
+import android.media.Image
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.soptstudy.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var diceImage: Image
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rollButton.text = "Let's Roll"
+        binding.rollButton.setOnClickListener {
+            rollDice()
+        }
     }
+
+    private fun rollDice() {
+        val raandomInt = Random().nextInt(6) + 1
+        val drawableResource = when (raandomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        binding.diceImage.setImageResource(drawableResource)
+    }
+
 }
