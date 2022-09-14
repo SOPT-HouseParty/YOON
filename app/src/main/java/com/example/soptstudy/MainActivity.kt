f03d1115b3a045a169fbe7d3c4760978174b3a1c
yoon <cbj0010@naver.com>
2022-09-14 17:19:18 +0900

    edittext

--- app/src/main/java/com/example/soptstudy/MainActivity.kt
+++ app/src/main/java/com/example/soptstudy/MainActivity.kt
@@ -7,25 +7,23 @@ import com.example.soptstudy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("Choi")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.button.setOnClickListener {
-            addNickName()
-        }
+        buttonClick()
     }
 
-    private fun addNickName() {
-
-        binding.apply {
-            tvShoew.text = binding.editTextTextPersonName.text
-            invalidateAll() //새로운걸 ui 업데이트 하기 위해서
-            editTextTextPersonName.visibility = View.GONE
-            button.visibility = View.GONE
-
+    private fun buttonClick(){
+        binding.doneButton.setOnClickListener {
+            with(binding){
+                nicknameText.text = nicknameEdit.text
+                nicknameEdit.visibility = View.GONE
+                doneButton.visibility = View.GONE
+                nicknameText.visibility = View.VISIBLE
+            }
         }
 
     }
